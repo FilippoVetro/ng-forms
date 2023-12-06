62b31d572b204788d63f18fde342d8bfa120c59b
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-06 12:46:57 +0100

    [ADD] sezione titolo di studio

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,33 +1,31 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {FormControl, NgForm} from "@angular/forms";
-import {debounceTime} from "rxjs";
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { FormControl, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
-  styleUrls: ['./template-driven.component.css']
+  styleUrls: ['./template-driven.component.css'],
 })
 export class TemplateDrivenComponent implements AfterViewInit {
-@ViewChild('code') code! : FormControl;
-  ngAfterViewInit() {
-    this.code.valueChanges.pipe(debounceTime(1000)).subscribe((value) => {
-      console.warn(value)
-    })
-    this.code.statusChanges.subscribe((status : string)=> {
-      console.log(status)
-    })
-  }
+  hasDegree: string = 'false';
+  degree: string = '';
+  inCorso: boolean = false;
+  annoConseguimento: string = '';
+  diploma: string = '';
+  annoConseguimentoDiploma: string = '';
+  certificazione: string = '';
+  dataScadenza: string = '';
+  @ViewChild('code') code!: FormControl;
+
+  ngAfterViewInit() {}
 
   onSubmit(myForm: NgForm) {
-    let productForm = myForm.form
-    console.log(productForm)
-    console.log(productForm.get('code'));
-    console.log(productForm.controls['qty']);
-    // SET VALUE
-    productForm.setValue({name: "Baguette", code: "bread-55", qty: "20", price: {priceBook: "book1"}})
-    // PATCH VALUE
-    productForm.patchValue({qty: "35"})
+    let productForm = myForm.form;
+    console.log(productForm);
+    console.log(productForm.controls);
+
     // RESET AI VALORI DI DEFAULT
-    // productForm.reset()
+    productForm.reset();
   }
 }
