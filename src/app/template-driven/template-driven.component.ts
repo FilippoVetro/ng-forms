04b223622a5fb690685a6cb67771424c635c1bdb
abFilippo <Filippo.vetro.3d@gmail.com>
2023-12-06 16:58:12 +0100

    Merge code

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { FormControl, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
@@ -17,8 +17,44 @@ export class TemplateDrivenComponent implements AfterViewInit {
   certificazione: string = '';
   dataScadenza: string = '';
   @ViewChild('code') code!: FormControl;
+  anagraficaForm!: FormGroup;
 
+  ngOnInit() {
+    this.anagraficaForm = new FormGroup({
+      nome: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+      ]),
+      cognome: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(75),
+      ]),
+      dataNascita: new FormControl('', Validators.required),
+      luogoNascita: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      codiceFiscale: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^([A-Z]{6})(\d{2})([A-Z])(\d{2})([A-Z])(\d{3})([A-Z])$/
+        ),
+      ]),
+      sesso: new FormControl('', Validators.required),
+      altezza: new FormControl(''),
+      nazionalita: new FormControl('', Validators.required),
+    });
+  }
   ngAfterViewInit() {}
+
+  submitAnagraficaForm() {
+    if (this.anagraficaForm.valid) {
+      console.log(this.anagraficaForm.value);
+      this.anagraficaForm.reset();
+    }
+  }
 
   onSubmit(myForm: NgForm) {
     let productForm = myForm.form;
